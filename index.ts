66956a2cdb8e8e5613375bf9a0ea35da63f7aa4a
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2022-03-06 11:42:43 +0100

    connect to mongoDB

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use('/', express.static('public'));
 
-app.listen(5000, () => {
-  console.log('listening to 5000');
-});
+const serve = async () => {
+  await mongoose.connect(process.env.MONGO_CONNECTION ?? '');
+
+  app.listen(5000, () => {
+    console.log('listening to 5000');
+  });
+};
+
+serve();
