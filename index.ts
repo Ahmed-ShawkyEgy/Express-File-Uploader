1b60f7aa7b79e45b61f23f2419f7c9a696af9609
Ahmed-ShawkyEgy <ahmed.shawkyegy@gmail.com>
2022-03-06 23:06:34 +0100

    update env var

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import filesRouter from './src/routes/fileRoutes';
 import fs from 'fs';
 import path from 'path';
 
+const PORT_NUMBER = process.env.PORT || 500;
+
 const app = express();
 
 app.use('/', express.static('public'));
@@ -21,9 +23,8 @@ try {
 
 const serve = async () => {
   await mongoose.connect(process.env.MONGO_CONNECTION ?? '');
-
-  app.listen(5000, () => {
-    console.log('listening to 5000');
+  app.listen(PORT_NUMBER, () => {
+    console.log(`listening to ${PORT_NUMBER}`);
   });
 };
 
